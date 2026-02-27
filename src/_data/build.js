c237ea3b7d72b93df222f6da962909ca564bdacb
mylo <milaniacardona@gmail.com>
2026-02-27 15:08:43 -0800

    more styles

--- src/_data/build.js
+++ src/_data/build.js
@@ -2,6 +2,8 @@ const versionFromSha = process.env.GITHUB_SHA
   ? process.env.GITHUB_SHA.slice(0, 8)
   : "";
 
+const localBuildVersion = `dev-${Date.now().toString(36)}`;
+
 module.exports = {
-  assetVersion: versionFromSha || "dev"
+  assetVersion: versionFromSha || localBuildVersion
 };
